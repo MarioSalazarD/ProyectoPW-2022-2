4f1264b81d04fcc0e1b398b9cf88a2b8a295a4b4
diego <diegofabian2002@hotmail.com>
2022-10-11 15:05:48 -0500

    actualizando review

--- src/Componentes/pages/Review.jsx
+++ src/Componentes/pages/Review.jsx
@@ -4,16 +4,47 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {NavBar, Footer} from '../shared';
 import Button from 'react-bootstrap/Button';
-
-
-
+import 'bootstrap/dist/css/bootstrap.css';
+import nate from '../img/nate.jpg';
+import influencer1 from '../img/influencer1.jpg';
+import influencer2 from '../img/influencer2.jpg';
+import './Review.css'
 const Review = () => {
     return(
         <Container className="vh-100 d-flex flex-column ">
             <Row>
                 <NavBar></NavBar>
             </Row>
-            
+
+            <div className="row " >
+            <div className="col-lg-3" >
+              <h2 className="kind">Nate Gentile</h2>
+              <div className="row" id="influencer">
+              <img src={nate} alt="horse" className="influencer"  />
+                VIDEO
+                </div>
+
+              </div>
+            <div className="col-lg-3" >
+              <h2 className="kind">Linux Tech</h2>
+              <div className="row" id="influencer">
+              <img src={influencer1} alt="horse" className="influencer" />
+
+              VIDEO
+                </div>
+
+
+                </div>
+             <div className="col-lg-3" >
+              <h2 className="kind">PewDiePie</h2>
+              <div className="row" id="influencer">
+              <img src={influencer2} alt="horse" className="influencer" />
+
+              VIDEO
+                </div>
+              </div>
+              </div>
+
         </Container>
     )
 }
