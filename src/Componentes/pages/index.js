4f1eecc826f8052f8aa5ce9fc0e8226829f4198a
diego <diegofabian2002@hotmail.com>
2022-10-11 13:45:20 -0500

    Añadiendo navbar-review

--- src/Componentes/pages/index.js
+++ src/Componentes/pages/index.js
@@ -1,3 +1,4 @@
 export {default as HomePage} from './HomePage'
 export {default as Login} from './Login'
 export {default as ProfileInfo} from './ProfileInfo'
+export {default as Review} from './Review'
