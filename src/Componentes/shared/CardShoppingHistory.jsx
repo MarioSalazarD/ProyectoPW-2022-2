466a491be852a8a21c816c03186497f5ac36c99a
diego <diegofabian2002@hotmail.com>
2022-10-14 19:36:40 -0500

    finalizando history review userreview ranking

--- src/Componentes/shared/CardShoppingHistory.jsx
+++ src/Componentes/shared/CardShoppingHistory.jsx
@@ -10,18 +10,27 @@ const CardShoppingHistory = (props) => {
     
   const { id, title, srcimg, description, date1 } = props;
   return(
-    <Card style={{ width: '100rem', 'margin-left': '2rem', 'margin-bottom': '0.5rem' }}>
+/*
+    <div className="row " >
+    <div className="col-lg-3" >
+      <h2 className="kind">Nate Gentile</h2>
+      <div className="row" id="influencer">
+      <img src={nate} alt="horse" className="influencer"  />
+        VIDEO
+        </div>
+        */
+
+
+    <Card style={{ width: '67rem', 'margin-left': '2rem', 'margin-bottom': '0.5rem' }}>
     <Row className="g-0">
     <Col md={4}>   
         <Card.Img style={{width: '5rem', 'margin-left': '10rem'}} variant="top" src={ srcimg } />
     </Col> 
     <Col md={8}>
         <Card.Body>
-        <Card.Title style={{ width: '70rem', 'margin-left': '2rem' }} > {title} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{description}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;{date1}</Card.Title>
+        <Card.Title style={{ width: '70rem', 'margin-left': '2rem', 'text-align': 'left' }} > {title} </Card.Title>
+        <Card.Title style={{ width: '70rem', 'margin-left': '2rem', 'text-align': 'left' }} > {description} </Card.Title>        
+        <Card.Title style={{ width: '70rem', 'margin-left': '2rem', 'text-align': 'left' }} > {date1}</Card.Title>
       <Card.Text>
       </Card.Text>
     </Card.Body>
