50afd3aac4c23a5de0ffa2e2f47446d4233c944b
Angelo Medina <20193065@aloe.ulima.edu.pe>
2022-10-15 11:38:05 -0500

    Agregando estilos

--- src/Componentes/shared/CardCar.jsx
+++ src/Componentes/shared/CardCar.jsx
@@ -5,7 +5,7 @@ import React from "react"
 
 const CardCar = (props) => {
     return (
-    <Card style={{ width: '24rem', 'margin-left': '30rem', 'margin-top': '13.5rem' , backgroundColor : '#5a6978'}}>
+    <Card style={{ width: '33rem', 'margin-left': '44rem', 'margin-top': '16rem' , backgroundColor : '#5a6978'}}>
       <Card.Img style={{ padding: '2rem', paddingBottom : '.5rem'}} variant="top" src={props.item.imgsrc}/>
       <Card.Body>
       <Button className = "button" variant="primary" style={{ backgroundColor : '#c25be9', width : '12rem', borderColor : '#c25be9' , borderRadius : '4px', marginLeft : '5rem', marginBottom : '1rem'}}>Add to the car</Button>
