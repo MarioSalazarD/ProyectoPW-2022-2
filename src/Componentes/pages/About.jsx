b6dc8ed2029a9f377df94a065730fe8198867b96
Jesus <jesus-10009@hotmail.com>
2022-10-15 12:50:47 -0500

    a15

--- src/Componentes/pages/About.jsx
+++ src/Componentes/pages/About.jsx
@@ -18,16 +18,18 @@ const About = () => {
 </Row >
 
 <Row className="medio" > 
-<h1 className="titulo"> CHECKOUT OUR LATEST BUILD AND MAKE IT YOUR OWN!</h1>
+<h1  style={{marginLeft:'-0px' , color:'black'}} className="titulo"> CHECKOUT OUR LATEST BUILD AND MAKE IT YOUR OWN!</h1>
 
 <div/>
 
-<img className="pc" src="/img/pc.png"  />
-<h2 className="titulo2" >OUR MISSION IS TO BUILD GAMING PCs AT THE BEST PRICE WITHOUT CUTTING CORNERS </h2>
+<img style={{marginLeft:'0px', marginTop:'200px' }} className="pc" src="https://cdn.originpc.com/img/pdp/gaming/desktops/genesis/case-7000d-airflow.png"  />
+<div style={{marginLeft:'100px', marginTop:'-250px' }}>
+<h2  className="titulo2" >OUR MISSION IS TO BUILD GAMING PCs AT THE BEST PRICE WITHOUT CUTTING CORNERS </h2>
+</div>
 </Row>
 
 <Row className="abajo">
-<Button className="botonConstruir" href="#">Let's Build</Button>
+<Button href="/recomend" className="botonConstruir" >Let's Build</Button>
 </Row>
 
 </Container>
