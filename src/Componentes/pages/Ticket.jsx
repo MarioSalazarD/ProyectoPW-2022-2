81dc1752b6ab5b859ea559cf17bfd86bdfb0a049
MarioSalazarD <20153314@aloe.ulima.edu.pe>
2022-12-03 04:50:46 -0500

    a

--- src/Componentes/pages/Ticket.jsx
+++ src/Componentes/pages/Ticket.jsx
@@ -7,6 +7,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from 'react'
 
 
 
